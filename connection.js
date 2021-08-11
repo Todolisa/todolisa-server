a6477195bdd06e0328e91103acd1b394c70aa371
ahmedaidev <52345388+ahmedaidev@users.noreply.github.com>
2021-08-11 23:49:02 +0200

    FIX(db): Database URL

--- connection.js
+++ connection.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 sequelize
-	.authenticate()
-	.then(() => [console.log('[+] Database connected')])
-	.catch(() => {
-		console.error('[!] Unable to connect to the database:', err)
-	})
+  .authenticate()
+  .then(() => {
+    console.log('[+] Database connected')
+  })
+  .catch(err => {
+    console.error('[!] Unable to connect to the database:', err)
+  })
 
 module.exports = sequelize
