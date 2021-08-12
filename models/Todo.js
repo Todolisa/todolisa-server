9a0c834123c7fb94ed93329d0c3a234d0bf0da97
ahmedaidev <52345388+ahmedaidev@users.noreply.github.com>
2021-08-12 11:53:09 +0200

    FEAT: Add migrate npm script

--- models/Todo.js
+++ models/Todo.js
@@ -2,22 +2,22 @@ const Sequelize = require('sequelize')
 const sequelize = require('../connection')
 
 module.exports = sequelize.define('todo', {
-	id: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-		autoIncrement: true,
-		primaryKey: true,
-	},
-	title: {
-		type: Sequelize.STRING,
-		unique: true,
-		allowNull: false,
-	},
-	completed: {
-		type: Sequelize.BOOLEAN,
-		defaultValue: false,
-	},
-	dueDate: Sequelize.STRING,
-	dueTime: Sequelize.STRING,
-	userId: Sequelize.INTEGER,
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  completed: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  dueDate: Sequelize.STRING,
+  dueTime: Sequelize.STRING,
+  userId: Sequelize.INTEGER,
 })
